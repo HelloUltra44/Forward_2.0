c7bba1a948a4249d720ea6979190c14d546aaeab
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-30 09:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "BQCWmGI1a9zqqIg9kgUQ6ZoAShV53TcpPw0YRM2zPjfaCacWXa4H7FXgDPbktV9-YE-DV3O5iyqSpbNGDDQsny0ieHHlQkWmskf_tNBI3OCfCFy5wi6K2YB2duy64tDEcz3TTpSClc8XFRIiIHTdygFguDa9ro4aWFfuW07JQCNYjCPwdsgOf7SpdbCh_VAPGAWEfPHAZC2r9ChQ72E35FpohpT5QG3ag10bMAOkTG5HU5xtVpUjx2rmWk7nOgBbEX3q18501ZJVTYlYBHEZeioF0ogFGi7rck7G8wgyOkHkxj6MgZhnQMPn6R1s3tM6XgWkj_lWW0_p2GLTNhdN5vUKAAAAAUKBF3YA")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001615615101"))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "fordaranbot"
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "fordaranbot")
 
 
 def LOGGER(name: str) -> logging.Logger:
